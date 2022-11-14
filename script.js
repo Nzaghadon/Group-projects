784eebbdc6e4ebd02fc31a17c2a4e4b82bce37f9
Nzagha Ikechukwu <100885888+Nzaghadon@users.noreply.github.com>
2022-11-15 00:37:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const words = document.getElementById('input');
+const count = document.getElementById('count');
+
+
+words.addEventListener('input', function countWord() {
+    let res = [];
+    let str = this.value.replace(/[\t\n\r\.\?\!]/gm, " ").split(" ");
+    str.map((s) => {
+        let trimStr = s.trim();
+        if (trimStr.length > 0) {
+            res.push(trimStr);
+        }
+    })
+    count.innerText = res.length;
+})
+
+
